71931367d4d98683394dd886a2a1362008735030
Resonious <metreckk@gmail.com>
2014-10-29 10:35:55 -0400

    (159) Order confirmation view is filled in

--- app/helpers/spree/google_trusted_store_helper.rb
+++ app/helpers/spree/google_trusted_store_helper.rb
@@ -1,5 +1,12 @@
 module Spree
   module GoogleTrustedStoreHelper
-    
+    def order_fields
+      %i(id domain email country currency total discounts shipping_total 
+        tax_total est_ship_date est_delivery_date has_preorder has_digital)
+    end
+
+    def item_fields
+      %i(item_name item_price item_quantity)
+    end
   end
 end
--- spec/views/spree/google_trusted_store/_order_confirmation.html.erb_spec.rb
+++ spec/views/spree/google_trusted_store/_order_confirmation.html.erb_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe 'spree/google_trusted_store/_order_confirmation.html.erb', order_spec: true, story_159: true do
   def render!(locals = {})
-    render partial: 'spree/google_trusted_store/badge', locals: locals
+    render partial: 'spree/google_trusted_store/order_confirmation', locals: locals
   end
 
   it 'displays START and END Google Trusted Stores Order' do
     render!
     expect(rendered).to start_with '<!-- START Google Trusted Stores Order -->'
-    expect(rendered).to start_with "<!-- END Google Trusted Stores Order -->\n"
+    expect(rendered).to end_with "<!-- END Google Trusted Stores Order -->\n"
   end
 
   %w(id domain email country currency total discounts shipping-total 
     tax-total est-ship-date est-delivery-date has-preorder has-digital
   ).each do |field|
     instance_eval <<-RUBY, __FILE__, __LINE__ + 1
-      context 'when passed #{field.underscore}' do
+      context 'when passed #{field.underscore}', order_field: true do
         it 'renders a span with the gts-o-#{field} that contains the #{field}' do
           render! #{field.underscore}: 'test-val'
-          expect(rendered).to have_css 'span##{field}', text: 'test-val'
+          expect(rendered).to have_css 'span#gts-o-#{field}', text: 'test-val'
         end
       end
     RUBY
   end
 
-  context 'line items' do
+  context 'line items', item_field: true do
     it 'renders a .gts-item span for each entry' do
       render! items: [{}, {}]
       expect(rendered).to have_css 'span.gts-item', count: 2
     end
 
-    %w(item_name item_price item_quantity).each do |field|
+    %w(item-name item-price item-quantity).each do |field|
       instance_eval <<-RUBY, __FILE__, __LINE__ + 1
         context 'given #{field.underscore}' do
           it 'renders #{field} for the item' do
